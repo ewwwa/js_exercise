0386f8c0baaafbd7bd9efca90f5ae5242d8e5237
ewwwa <eva.anguitalopez@gmail.com>
2016-05-08 20:22:19 +0200

    reindented lines

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 //selector fijo cuando se clica
-	$('ul:first li a').click(function() {
+$('ul:first li a').click(function() {
 	//se quitan los todos atributos de la clase para luego añadirlo 
 	$('.current-menu-item').removeClass();
 	$(this).parent().addClass('current-menu-item');
 
-	});
+});
 });
